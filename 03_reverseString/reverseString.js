898712cb4922230293b04bf76b3701c2f8241bea
Karla Rosas <k2.rosas.v@gmail.com>
2024-01-01 16:02:03 -0600

    Resolved reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let words = str.split(' ');
+    let reversedString = words.map((word) => word.split('').reverse().join("")).reverse().join(" ")
 
-};
+    return reversedString;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
