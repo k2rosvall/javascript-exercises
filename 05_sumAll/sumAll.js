6636852452892020a3bdc354ad6d37a609a4e223
Karla Rosas <k2.rosas.v@gmail.com>
2024-01-01 16:35:26 -0600

    Resolve sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(firstValue, secondValue) {
+    let values = Array.from(arguments);
+    if (values.some((value) => typeof value != "number" || value < 0 )) return "ERROR";
+    
+    return firstValue > secondValue ? applyFormula(firstValue) : applyFormula(secondValue);
 };
+
+function applyFormula(value) {
+    return (value * (value + 1) ) / 2;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
