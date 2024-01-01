3d9c70c0003ea2d03cc0196da139f09568563817
Karla Rosas <k2.rosas.v@gmail.com>
2024-01-01 17:01:40 -0600

    Resolve tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let newTemp = ((tempF - 32)  * 5) / 9;
+  return roundNumber(newTemp);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let newTemp = ((tempC * 9)/ 5) + 32
+  return roundNumber(newTemp);
 };
 
+function roundNumber(number) {
+  return Number((Math.round(number * 10) / 10).toFixed(1));
+}
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
