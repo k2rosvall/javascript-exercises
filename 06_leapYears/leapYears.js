b0820fb73b0d42822489b9196a378123bfe6d9ac
Karla Rosas <k2.rosas.v@gmail.com>
2024-01-01 16:52:36 -0600

    Resolve leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let condition1 = year % 4 === 0 && year % 100 != 0;
+    let condition2 = year % 4 === 0 && year % 100 === 0 && year % 400 === 0;
 
+    return condition1 || condition2 ? true : false
 };
 
 // Do not edit below this line
